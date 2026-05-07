fca04602970cb280f3c6c1fe32052ae6e1ca14b4
Tran Thien Nhan <nhan_tt@vietvang.net>
2026-05-07 21:06:51 +0700

    Add Google Tag Manager (GTM-5WC4X849)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
@@ -73,6 +74,15 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased bg-black`}
     >
       <body className="min-h-full flex flex-col bg-black text-white [overscroll-behavior:contain]">
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5WC4X849"
+height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
+        <Script id="gtm" strategy="beforeInteractive" dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5WC4X849');`,
+        }} />
         <Navbar />
         {children}
         <Footer />
